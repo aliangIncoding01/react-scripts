ca66da597821225492e2f88980109c9c1e0ce471
liuxin01 <lxin131@163.com>
2020-03-28 20:23:10 +0800

    feat: 支持mock接口

---
+++ my-template/template/mock/user/list.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'POST /user/list': {
+        code: 0,
+        msg: '请求成功',
+        data: [
+            {id: 1, name: 'cacheLayer', age: 29},
+            {id: 2, name: 'coco', age: 3},
+        ],
+        ts: 1547193609
+    },
+};
--- my-template/template/src/App.js
+++ my-template/template/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import './test';
 
 function App() {
+  useEffect(() => {
+    post('/user/list');
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -26,6 +30,25 @@ function App() {
       </header>
     </div>
   );
+}
+
+function post(url) {
+  const xhr = new XMLHttpRequest();
+  const data = new FormData();
+  data.append('id', 12345);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        const responseText = xhr.responseText || '{}';
+        const res = JSON.parse(responseText);
+        if (res && res.code === 0) {
+          console.log(res.data);
+        }
+    }
+  };
+
+  xhr.open('POST', url);
+  xhr.send(data);
 }
 
 export default App;
