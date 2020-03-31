2def232d928a1a4e284816bd82a71c4eda7aa61c
liuxin01 <lxin131@163.com>
2020-03-31 23:48:37 +0800

    feat: 封装redux，支持状态管理

--- my-template/template/src/router/router.config.js
+++ my-template/template/src/router/router.config.js
@@ -1,6 +1,7 @@
 // demo页面
 const demo = () => import('@/page/demo');
 const demo_mock = () => import('@/page/demo/mock');
+const demo_store = () => import('@/page/demo/store');
 
 const routes = [
     // demo页面
@@ -13,6 +14,14 @@ const routes = [
                 component: demo_mock,
                 onEnter: () => {
                     document.title = 'demo-mock';
+                },
+                onLeave: () => { }
+            },
+            {
+                path: '/demo/store',
+                component: demo_store,
+                onEnter: () => {
+                    document.title = 'demo-store';
                 },
                 onLeave: () => { }
             }
